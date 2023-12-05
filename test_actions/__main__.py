74718aa485ef65f3f304f8bb1197b841142f2095
david-why <david_why@outlook.com>
2023-12-05 20:12:19 +0800

    update stuff

--- test_actions/__main__.py
+++ test_actions/__main__.py
@@ -1,4 +1,12 @@
+import atexit
 import os
+import pkg_resources
 
 print(os.getcwd())
 print(os.listdir())
+
+path = pkg_resources.resource_filename(__package__, 'static')
+
+print(os.listdir(path))
+
+atexit.register(pkg_resources.cleanup_resources)
