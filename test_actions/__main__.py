31dfe5904adf726efa4db675dad9ea34f945f750
david-why <david_why@outlook.com>
2023-12-06 21:23:40 +0800

    add bunch of requirements

--- test_actions/__main__.py
+++ test_actions/__main__.py
@@ -1,6 +1,11 @@
 import os
 from importlib.resources import files
 from markupsafe import Markup
+import dotenv
+from redis.asyncio import from_url
+from uvicorn.main import main
+from quart import Quart
+from aiofiles import open as aioopen
 
 print(Markup('<b>%s</b>') % 'test this & that')
 
